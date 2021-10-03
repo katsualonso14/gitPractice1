a6bd4eb052a2831d8c69a19ca7e52fa26f92f063
KaysuyaTamai <xabi.k0124football@gmail.com>
2021-10-03 17:22:00 +0900

    autlayout_test

--- ViewController.swift
+++ ViewController.swift
@@ -1,5 +1,4 @@
-// ボタンをタップして音を出す
-// 中国語音声読み上げ
+//layout test
 //  TestApp
 
 import UIKit
@@ -7,56 +6,57 @@ import AVFoundation
  
 class ViewController: UIViewController, AVAudioPlayerDelegate, AVSpeechSynthesizerDelegate {
     
-    var audioPlayer:AVAudioPlayer!
-    let  synthesizer = AVSpeechSynthesizer()
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let audioSession = AVAudioSession.sharedInstance()
-        
-        do {
-     
-//            // デリゲートの設定
-//            audioPlayer!.delegate = self
-            // マナーモードでも音を鳴らすようにする
-            try audioSession.setCategory(.playback)
-
-        } catch {
-            print("Audio Setting Failed.")
-            return
-        }
-
-        
         button()
+        button2()
         
         view.backgroundColor = .white
-    
-        
+        view.translatesAutoresizingMaskIntoConstraints = false
     }
-    
  
     
     @objc func PushButton(sender: UIButton) {
-        
-        let utterance = AVSpeechUtterance.init(string: "你好")
-        let voice = AVSpeechSynthesisVoice.init(language: "zh-CN")
-        utterance.voice = voice
-    synthesizer.speak(utterance)
-        
         print("tap")
     }
     
     func button() {
-        let button: UIButton = UIButton(frame: CGRect(x: 70, y: 150, width: 300, height: 150))
+        let button: UIButton = UIButton()
+        self.view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .red
+//        connstraitでlayoutを調整する
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        button.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.6).isActive = true
+        button.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.2).isActive = true
+        
         button.setTitle("你好", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.addTarget(self, action: #selector(PushButton), for: .touchUpInside)
-        self.view.addSubview(button)
+        
     }
     
-    
+    func button2() {
+        let button2: UIButton = UIButton()
+        self.view.addSubview(button2)
+        button2.translatesAutoresizingMaskIntoConstraints = false
+        button2.backgroundColor = .red
+//        connstraitでlayoutを調整する
+        button2.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100.0).isActive = true
+        button2.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button2.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        button2.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.6).isActive = true
+        button2.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.2).isActive = true
+        
+        button2.setTitle("你好", for: .normal)
+        button2.setTitleColor(.black, for: .normal)
+        button2.addTarget(self, action: #selector(PushButton), for: .touchUpInside)
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
 }
 		
